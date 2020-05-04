dc000a8379798b8e2227d0ac3a44fb2b0571eecf
luissncs <luissncs@gmail.com>
2020-05-04 07:06:01 -0400

    jenkins file update

--- kubernetes_deploy.sh
+++ kubernetes_deploy.sh
@@ -1,6 +1,7 @@
 ##!/usr/bin/env bash
 # Should be switched to ubuntu user.
 echo $USER
+echo $WORKSPACE
 # Delete existing deployment on kubernetes.
 kubectl delete -f swe645-hw2-kubernetes.yaml
 # Deploy updated images
